69cc30ef1db0a84ab06d0fdcd125f4049137fbc5
JojokCreator <joeyk2k@hotmail.com>
2022-07-04 17:56:13 +0100

    css adjust

--- src/Components/List/ListItem/index.js
+++ src/Components/List/ListItem/index.js
@@ -1,16 +1,18 @@
 import React from 'react'
-import { Checkbox, Button, List } from 'semantic-ui-react'
+import { Checkbox, Button, List, Segment } from 'semantic-ui-react'
 
 const ListItem = (props) => {
     return (
 
+        <Segment raised>
         <List.Item id='todo'>
-            <Checkbox checked={props.tick} onChange={props.handleCheck} type='checkbox'></Checkbox>
+            <Checkbox label='Done' checked={props.tick} onChange={props.handleCheck} type='checkbox'></Checkbox>
             <h3 id="todo-name">{props.item}</h3>
             <div>
             <Button floated='right' color="red "onClick={props.deleteToDo}>Delete this todo</Button>
             </div>
         </List.Item>
+            </Segment>    
 
     )
 }

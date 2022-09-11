e584a05c036ab757d698e16baede733f00c04fb5
Joe Klakus <joeyk2k@hotmail.com>
2022-09-11 14:56:20 +0100

    dependencies

--- src/Components/List/ListItem/index.js
+++ src/Components/List/ListItem/index.js
@@ -1,20 +1,19 @@
 import { MdDeleteForever } from 'react-icons/md';
 
 const ListItem = (props) => {
-    console.log(props)
     return (
         <div className="todo">
-            <label class="checkbox-container">
+            <label className="checkbox-container">
             <input className="todo-checkbox" type="checkbox"
              checked={props.tick} onChange={props.handleCheck} />
-            <span class="checkmark"></span>
+            <span className="checkmark"></span>
             </label>
             <div className="list-container">
-            <p class="todo-body" style={{ textDecoration: (props.tick === true) ? "line-through" : "none" }}>{props.item}</p>
+            <p className="todo-body" style={{ textDecoration: (props.tick === true) ? "line-through" : "none" }}>{props.item}</p>
             <div className="todo-delete">
             <MdDeleteForever onClick={props.deleteToDo}/>
             </div>
-                {/* <button class="todo-delete" onClick={props.deleteToDo}>Delete</button> */}
+                {/* <button className="todo-delete" onClick={props.deleteToDo}>Delete</button> */}
             </div>
         </div>
     )
